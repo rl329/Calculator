cd85c66698113634416b98e7dc85a437c19f0ca3
rl329 <rl329@njit.edu>
2024-09-28 03:02:16 -0400

    added all changes

---
+++ calculator/calculator.py
@@ -0,0 +1,39 @@
+from typing import List, Callable
+from calculator.operations import add, subtract, multiply, divide
+from calculator.calculation import Calculation
+
+class Calculator:
+    history: List[Calculation] = []
+
+    operations_map = {
+        "add": add,
+        "subtract": subtract,
+        "multiply": multiply,
+        "divide": divide,
+    }
+
+    @classmethod
+    def perform_operation(cls, a: float, b: float, operation_name: str) -> float:
+        try:
+            operation_func = cls.operations_map[operation_name]
+        except KeyError:
+            raise ValueError("Error operation")
+        
+        result = operation_func(a, b)
+        
+        calculation = Calculation(a, b, operation_name, result)
+        cls.history.append(calculation)
+        
+        return result
+
+    @classmethod
+    def get_history(cls) -> List[Calculation]:
+        return cls.history
+
+    @classmethod
+    def clear_history(cls) -> None:
+        cls.history.clear()
+
+    @classmethod
+    def get_latest_calculation(cls) -> Calculation:
+        return cls.history[-1] if cls.history else None
