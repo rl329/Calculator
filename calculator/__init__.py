444e56d7a2bdb2a14cda9cf8b332c8612810fea7
rl329 <rl329@njit.edu>
2024-09-28 01:10:54 -0400

    correct _init_ and calculations

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,37 +1,30 @@
-from calculator.calculations import Calculations
-from calculator.operations import add, subtract, multiply, divide
 from calculator.calculation import Calculation
-from decimal import Decimal
-from typing import Callable
+from calculator.operations import add, subtract, multiply, divide
 
 class Calculator:
     @staticmethod
-    def _perform_operation(a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]) -> Decimal:
+    def add (a,b):
         """ Create and perform a calculation, then return the result. """
-        calculation = Calculation.create(a, b, operation)
-        Calculation.add_calculation(calculation)
-
-        return calculation.perform()
+        calculation = Calculation(a, b, add)
+        return calculation.get_result()
     
     @staticmethod
-    def add(a: Decimal, b: Decimal) -> Decimal:
-        """Perform addition using the _perform_operation method."""
-        return Calculator.perform_operation(a, b, add)
-    
-    @staticmethod
-    def subtract(a: Decimal, b: Decimal) -> Decimal:
+    def subtract(a, b):
         """Perform subtraction using the _perform_operation method."""
-        return Calculator._perform_operation(a, b, subtract)
+        calculation = Calculation(a, b, subtract)
+        return calculation.get_result()
     
     @staticmethod
-    def multiply(a: Decimal, b: Decimal) -> Decimal:
+    def multiply(a,b):
         """Perform multiplication using the _perform_operation method."""
-        return Calculator._perform_operation(a, b, multiply)
+        calculation = Calculation(a, b, multiply)
+        return calculation.get_result()
     
     @staticmethod
-    def divide(a: Decimal, b: Decimal) -> Decimal:
+    def divide(a, b):
         """Perform division using the _perform_operation method."""
-        return Calculator._perform_operation(a, b, divide)
+        calculation = Calculation(a, b, divide)
+        return calculation.get_result()
     
 
 
--- calculator/calculation.py
+++ calculator/calculation.py
@@ -1,20 +1,8 @@
-from decimal import Decimal
-from typing import Callable
-
 class Calculation:
-    def __init__(self, a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]) -> None:
+    def __init__(self, a, b, operation):
         self.a = a
         self.b = b
         self.operation = operation
         
-        @staticmethod
-        def create(a: Decimal, b: Decimal, operation: Callable[[Decimal, Decimal], Decimal]):
-            return Calculation(a, b, operation)
-        
-        def perform_calculation(self) -> Decimal:
-            return self.operation(self.a, self.b)
-
-        def __repr__(self) -> str:
-            return f"Calculation({self.a}, {self.b}, {self.operation.__name__})"
-        
-            
+def get_result(self):
+    return self.operation(self.a, self.b)
